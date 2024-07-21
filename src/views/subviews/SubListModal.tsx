9a0cb8b873e6d4d14d4487be39aacace7dbb04be
hunterchan <643948951@qq.com>
2024-07-21 12:02:10 +0800

    update

--- src/views/subviews/SubListModal.tsx
+++ src/views/subviews/SubListModal.tsx
@@ -9,6 +9,7 @@ export default function SubListModal({problemID}: { problemID: number }) {
     const [dataSource, setDataSource] = useState<BeanSubmit[]>(
         []
     );
+    const [solved, setSolved] = useState('red');
 
     const columns = [
         {
@@ -24,28 +25,37 @@ export default function SubListModal({problemID}: { problemID: number }) {
     ]
 
     useEffect(() => {
-            axios.get(API_URL + '/submit/' + localStorage.getItem('userid') + '/' + problemID, {
-                headers: {
-                    Authorization: localStorage.getItem('token')
-                }
-            })
-                .then((response) => {
-                    if (response.data.code === 0) {
-                        setDataSource(response.data.data)
-                    } else {
-                        alert(response.data.msg);
+        axios.get(API_URL + '/submit/' + localStorage.getItem('userid') + '/' + problemID, {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        })
+            .then((response) => {
+                if (response.data.code === 0) {
+                    setDataSource(response.data.data)
+
+                    //遍历response.data.data查找有没有state为'通过'
+                    for (let i = 0; i < response.data.data.length; i++) {
+                        if (response.data.data[i].state === '通过') {
+                            setSolved('green');
+                            break;
+                        }
                     }
-                }).catch((error) => {
-                alert(error);
-            })
-        }, [setDataSource, problemID]);
+
+                } else {
+                    alert(response.data.msg);
+                }
+            }).catch((error) => {
+            alert(error);
+        })
+    }, [setDataSource, problemID,setSolved]);
 
 
     return (
         <div>
-            <Button type="primary" danger={true} block={false} onClick={() => {
+            <Button type="primary" style={{backgroundColor: solved}} danger={true} block={false} onClick={() => {
                 setOpen(true)
-            }}>提交列表</Button>
+            }}>提交记录</Button>
             <Modal
                 title="提交列表"
                 open={open}
